8d719cb126881324899a9b8dbb6cbcd76c4fea47
pmurphy926 <patrick.murphy926@gmail.com>
2023-02-07 19:59:36 -0500

    mobile styling

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,6 +8,8 @@ const About = () => {
           <p>I am a full stack developer with a background in education and logistics. Through my years managing a shipping department and a classroom, I have developed invaluable skills in problem solving, collaboration, and making deadlines. I've worked with people of all different types in order to get things done well and on time - truck drivers, port managers, students, parents, teachers, and administrators. Working together toward a common goal is how great things happen, and I am looking forward to being part of a new team.</p>
           <br />
           <p>I'm a voracious reader, music listener, and movie watcher. I've spent a significant amount collecting and organizing my physical and digital collections, which is where my interest in software and data began. During the summer months, you'll find me following the Mets and other baseball minutiae with an obsessiveness that has been developing since first grade.</p>
+          <br />
+          <p>I'm very excited about learning and growing as a software developer, be it frontend, backend, or full stack. Please don't hesitate to reach out!</p>
         </div>
     </div>
   )

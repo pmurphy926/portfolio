ff3d06508c81bace903b35d8bd04bd371bd0dc6a
pmurphy926 <patrick.murphy926@gmail.com>
2023-02-08 10:46:44 -0500

    dropdown working on mobile

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,6 +4,13 @@ import { useNavigate } from 'react-router-dom'
 const Nav = (props) => {
     const navigate = useNavigate();
 
+    function toggleDropdown () {
+        const hamburger = document.getElementById("hamburger");
+        const dropdownContent = document.getElementById("myDropdown");
+        hamburger.classList.toggle("change");
+        dropdownContent.classList.toggle("show");
+    }
+
     return (
         <>
         <h3 className='header-h3'>Patrick Murphy</h3>
@@ -17,6 +24,22 @@ const Nav = (props) => {
                     <li> <button className='nav-btn' onClick={() => {navigate('/contact')}}><span className='li'> Contact</span></button> </li>
                 </ul>
             </nav>
+        </div>
+        <div id='mobile-nav-div'>
+            <div class="dropdown">
+                <div id="hamburger" class="dropBtn" onClick={toggleDropdown}>
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+                <div id="myDropdown" class="dropdown-content">
+                    <li> <button className='drop-nav-btn' onClick={() => {navigate('/')}}><span className='li'>Home</span></button> </li>
+                    <li> <button className='drop-nav-btn' onClick={() => {navigate('/about')}}><span className='li'>About</span></button> </li>
+                    <li> <button className='drop-nav-btn' onClick={() => {navigate('/skills')}}><span className='li'>Skills</span></button> </li>
+                    <li> <button className='drop-nav-btn' onClick={() => {navigate('/projects')}}><span className='li'>Projects</span></button> </li>
+                    <li> <button className='drop-nav-btn' onClick={() => {navigate('/contact')}}><span className='li'> Contact</span></button> </li>
+                </div>
+            </div>
         </div>
         </>
   )

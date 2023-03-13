d62d68a6b947aa10aaa2a0878049112d7e5a6aee
pmurphy926 <patrick.murphy926@gmail.com>
2023-03-13 15:28:15 -0400

    resume link updated

--- src/components/iconbar.jsx
+++ src/components/iconbar.jsx
@@ -7,7 +7,7 @@ const IconBar = () => {
             <a href="https://www.linkedin.com/in/patrickmurphy90/" class="linkedin" target="_blank">
                 <img className='icon-bar-icon' src="/linkedin.png" alt="LinkedIn logo" />
             </a>
-            <a href="https://drive.google.com/file/d/1EHcHGAkJW3j-A0nwdKc_tP57amlirT90/view?usp=share_link" class="resume" target='_blank'>
+            <a href="https://drive.google.com/file/d/14EkRgTilITQSJOh5YEccHva7uv5JA5XM/view?usp=share_link" class="resume" target='_blank'>
                 <img className='icon-bar-icon' src="/resume.png" alt="Document icon" />
             </a>
             <a href="https://github.com/pmurphy926" class="github" target="_blank">
